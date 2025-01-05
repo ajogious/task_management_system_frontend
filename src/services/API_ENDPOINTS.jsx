f116ab110756e8c9f871e37898ed15b7b3f71b24
ajogious <ajogious@gmail.com>
2025-01-05 05:01:40 +0100

    build

--- src/services/API_ENDPOINTS.jsx
+++ src/services/API_ENDPOINTS.jsx
@@ -2,35 +2,36 @@ const BASE_URL = import.meta.env.VITE_APP_API_URL;
 
 const API_ENDPOINTS = {
   AUTH: {
-    LOGIN: `${BASE_URL}/auth/login`,
-    LOGOUT: `${BASE_URL}/auth/logout`,
-    REGISTER: `${BASE_URL}/auth/register`,
-    FORGOT_PASSWORD: `${BASE_URL}/auth/forgot-password`,
-    RESET_PASSWORD: `${BASE_URL}/auth/reset-password`,
+    LOGIN: `${BASE_URL}/api/auth/login`,
+    LOGOUT: `${BASE_URL}/api/auth/logout`,
+    REGISTER: `${BASE_URL}/api/auth/register`,
+    FORGOT_PASSWORD: `${BASE_URL}/api/auth/forgot-password`,
+    RESET_PASSWORD: `${BASE_URL}/api/auth/reset-password`,
   },
   ADMIN: {
-    ADMIN_USERS_STATS: `${BASE_URL}/admin/dashboard/stats`,
-    USERS: `${BASE_URL}/admin/users`,
-    SUSPEND_USER: (userId) => `${BASE_URL}/admin/${userId}/suspend`,
-    ACTIVATE_USER: (userId) => `${BASE_URL}/admin/${userId}/activate`,
-    DELETE_USER: (userId) => `${BASE_URL}/admin/user/${userId}`,
+    ADMIN_USERS_STATS: `${BASE_URL}/api/admin/dashboard/stats`,
+    USERS: `${BASE_URL}/api/admin/users`,
+    SUSPEND_USER: (userId) => `${BASE_URL}/api/admin/${userId}/suspend`,
+    ACTIVATE_USER: (userId) => `${BASE_URL}/api/admin/${userId}/activate`,
+    DELETE_USER: (userId) => `${BASE_URL}/api/admin/user/${userId}`,
   },
   DASHBOARD: {
-    STATS: (userId) => `${BASE_URL}/dashboard/stats/${userId}`,
+    STATS: (userId) => `${BASE_URL}/api/dashboard/stats/${userId}`,
   },
   PROFILE: {
-    GET_PROFILE: (userId) => `${BASE_URL}/profile/${userId}`,
+    GET_PROFILE: (userId) => `${BASE_URL}/api/profile/${userId}`,
   },
   TASKS: {
-    POST_TASKS: `${BASE_URL}/tasks`,
-    GET_TASKS: (userId) => `${BASE_URL}/tasks/user/${userId}`,
+    POST_TASKS: `${BASE_URL}/api/tasks`,
+    GET_TASKS: (userId) => `${BASE_URL}/api/tasks/user/${userId}`,
     UPDATE_TASK: (userId, taskId) =>
-      `${BASE_URL}/tasks/user/${userId}/tasks/${taskId}`,
-    DELETE_TASK: (taskId) => `${BASE_URL}/tasks/user/${taskId}`,
+      `${BASE_URL}/api/tasks/user/${userId}/tasks/${taskId}`,
+    DELETE_TASK: (taskId) => `${BASE_URL}/api/tasks/user/${taskId}`,
   },
   USER: {
-    USER_PROFILE: (userId) => `${BASE_URL}/users/user/${userId}`,
-    UPDATE_PROFILE: (userId) => `${BASE_URL}/users/update-profile/${userId}`,
+    USER_PROFILE: (userId) => `${BASE_URL}/api/users/user/${userId}`,
+    UPDATE_PROFILE: (userId) =>
+      `${BASE_URL}/api/users/update-profile/${userId}`,
   },
 };
 
