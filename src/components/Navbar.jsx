0197c968aad00c39a49e1d0f7ca4792caf5026ab
ajogious <ajogious@gmail.com>
2025-01-05 12:15:29 +0100

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -118,7 +118,7 @@ function Navbar({ setMessage, setAlertType }) {
                     aria-label="User menu"
                   >
                     <img
-                      src={userImage || "default_avatar.png"}
+                      src={userImage || "avata.png"}
                       alt="User"
                       style={{
                         width: "35px",
